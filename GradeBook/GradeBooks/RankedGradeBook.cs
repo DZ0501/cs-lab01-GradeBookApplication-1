d7142f7cfcc98407f87900f23e8fac1ac1cdc3d8
Dawid Żmuda <dawid.zmuda@microsoft.wsei.edu.pl>
2022-03-17 19:28:26 +0100

    Override RankedGradeBook's CalculateStatistics method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -34,7 +34,17 @@ namespace GradeBook.GradeBooks
                 return 'D';
             else
                 return 'F';
+        }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+
+            base.CalculateStatistics();
         }
     }
 }
